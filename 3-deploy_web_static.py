fe3fbcc41765ba0c012fe10bb2956f5f9fd44a24
adiomi90 <adiomi90@outlook.com>
2023-12-16 10:25:09 -0800

    delpy static new version

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,59 +1,78 @@
 #!/usr/bin/python3
-""" A module that generates and distributes an archive to your web server """
-from fabric.api import put, run, local, env
-from time import strftime
-from os import path
+"""
+do_pack(): Generates a .tgz archive from the
+contents of the web_static folder
+do_deploy(): Distributes an archive to a web server
+deploy (): Creates and distributes an archive to a web server
+"""
+
+from fabric.operations import local, run, put
+from datetime import datetime
+import os
+from fabric.api import env
+import re
 
 
-env.hosts = ["54.90.63.173", "52.91.134.31"]
-env.user = "ubuntu"
-env.key_filename = "~/.ssh/school"
+env.hosts = ['54.90.63.173', '52.91.134.31']
 
 
 def do_pack():
-    """ A fabric function to generate the archive content """
-
-    timestamp = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(timestamp))
-
-        return "versions/web_static_{}.tgz".format(timestamp)
-
-    except Exception as err:
+    """Function to compress files in an archive"""
+    local("mkdir -p versions")
+    filename = "versions/web_static_{}.tgz".format(datetime.strftime(
+                                                   datetime.now(),
+                                                   "%Y%m%d%H%M%S"))
+    result = local("tar -cvzf {} web_static"
+                   .format(filename))
+    if result.failed:
         return None
+    return filename
 
 
 def do_deploy(archive_path):
-    """ A Fabric function to distribute the archive content"""
-
-    if not path.exists(archive_path):
+    """Function to distribute an archive to a server"""
+    if not os.path.exists(archive_path):
         return False
-    try:
-        arcfile = archive_path.split("/")[-1]
-        fname = arcfile.split(".")[0]
-        put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}/".format(fname))
-        run("tar -zxvf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(arcfile, fname))
-        run("rm /tmp/{}".format(arcfile))
-        run("mv /data/web_static/releases/{}/web_static/*\
-            /data/web_static/releases/{}/".format(fname, fname))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(fname))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(fname))
-        return True
-    except Exception as err:
+    rex = r'^versions/(\S+).tgz'
+    match = re.search(rex, archive_path)
+    filename = match.group(1)
+    res = put(archive_path, "/tmp/{}.tgz".format(filename))
+    if res.failed:
         return False
+    res = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    if res.failed:
+        return False
+    res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("rm /tmp/{}.tgz".format(filename))
+    if res.failed:
+        return False
+    res = run("mv /data/web_static/releases/{}"
+              "/web_static/* /data/web_static/releases/{}/"
+              .format(filename, filename))
+    if res.failed:
+        return False
+    res = run("rm -rf /data/web_static/releases/{}/web_static"
+              .format(filename))
+    if res.failed:
+        return False
+    res = run("rm -rf /data/web_static/current")
+    if res.failed:
+        return False
+    res = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+              .format(filename))
+    if res.failed:
+        return False
+    print('New version deployed!')
+    return True
 
 
 def deploy():
-    """ runs the 2 fabric functions """
-
-    path = do_pack()
-    if not path:
+    """Creates and distributes an archive to a web server"""
+    filepath = do_pack()
+    if filepath is None:
         return False
-
-    return do_deploy(path)
+    d = do_deploy(filepath)
+    return d
